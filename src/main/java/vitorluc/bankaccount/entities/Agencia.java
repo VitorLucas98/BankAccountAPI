a6503775c352870f7a11dee5d892cc36ae36835c
Vitor Lucas <vitorluc.2013@gmail.com>
2022-04-16 11:38:06 -0300

    Mudança do projeto para JPA, classe mapeadas

---
+++ src/main/java/vitorluc/bankaccount/entities/Agencia.java
@@ -0,0 +1,28 @@
+package vitorluc.bankaccount.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Agencia implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String endereco;
+
+}
---
+++ src/main/java/vitorluc/bankaccount/entities/Correntista.java
@@ -0,0 +1,31 @@
+package vitorluc.bankaccount.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Correntista implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+}
