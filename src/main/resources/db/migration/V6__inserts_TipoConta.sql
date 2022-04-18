d864bd672053af17703a519681febed3a158f777
Vitor Lucas <vitorluc.2013@gmail.com>
2022-04-18 10:56:47 -0300

    Organizando a estrutura do flyway

---
+++ src/main/resources/db/migration/V6__inserts_TipoConta.sql
@@ -0,0 +1,2 @@
+INSERT INTO tipo_conta (descricao) VALUES ('Comum');
+INSERT INTO tipo_conta (descricao) VALUES ('Especial');
---
+++ src/main/resources/db/migration/V8__inserts_ContaCorrente.sql
@@ -0,0 +1,3 @@
+INSERT INTO conta_corrente (id_correntista, id_agencia, id_tipo_conta, limite, saldo, ativa) VALUES (1, 2, 1, 1000.00, 0.00, 'F');
+INSERT INTO conta_corrente (id_correntista, id_agencia, id_tipo_conta, limite, saldo, ativa) VALUES (2, 1, 1, 15000.00, 4381.74, 'T');
+INSERT INTO conta_corrente (id_correntista, id_agencia, id_tipo_conta, limite, saldo, ativa) VALUES (3, 3, 2, 200000.00, 142893.89, 'T');
